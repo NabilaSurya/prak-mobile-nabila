6a640f7fa17df846574b1abbea52684abc30be3f
NabilaSurya <surya24si@mahasiswa.pcr.ac.id>
2026-04-10 08:23:15 +0700

    Pertemuan 3: penerapan binding

---
+++ app/src/main/java/com/example/nabeeyaps/pertemuan3/ThirdActivity.kt
@@ -0,0 +1,37 @@
+package com.example.nabeeyaps.pertemuan3
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.nabeeyaps.R
+import com.example.nabeeyaps.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+
+    private lateinit var inputNoTujuan: EditText
+    private lateinit var btnKirim: Button
+
+    private lateinit var binding: ActivityThirdBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnKirim.setOnClickListener {
+            val nomor = binding.inputNoTujuan.text.toString()
+
+            if (nomor.isNotEmpty()) {
+                Toast.makeText(this, "Pesan berhasil dikirim ke: $nomor", Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(this, "Nomor tidak boleh kosong", Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+}
